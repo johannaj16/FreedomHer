e9ca994e42ac17ce82cfd94813a71e83ec7b86f9
GabePalomino <gabepalominoc@gmail.com>
2024-02-03 17:01:05 -0500

    nav finally fixed

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,33 +1,36 @@
-import React, {useState} from "react";
-import AddPost from '../components/AddPost';
+import React, { useState } from "react";
+import AddPost from "../components/AddPost";
 
+function Home() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-        
-        function Home() {
-          const[isModalOpen, setIsModalOpen] = useState(false);
-          
-          return (
-            <div>
-              {/* Other content... */}
-              
-              <div className="text-[rgb(55,32,107)] flex flex-col items-center font-sans font-bold h-screen mx-8">
-        <div className="text-purple w-full text-[3.3rem] lg:text-[5.3rem] mt-[5rem] font-herfonty text-center py-20">FreedomHER</div>
-        <div className="w-full text-[2.3rem] font-herfonty text-center -mt-[3rem] mb-[9rem] drop-shadow-[rgba(0,0,0,.55)]">an anonymous forum to support domestic violence victims.</div>
+  return (
+    <div>
+      {/* Other content... */}
+
+      <div className="text-[rgb(55,32,107)] flex flex-col items-center font-sans font-bold h-screen mx-8">
+        <div className="text-purple w-full text-[3.3rem] lg:text-[5.3rem] mt-[5rem] font-herfonty text-center py-20">
+          FreedomHER
+        </div>
+        <div className="w-full text-[2.3rem] font-herfonty text-center -mt-[3rem] mb-[9rem] drop-shadow-[rgba(0,0,0,.55)]">
+          an anonymous forum to support domestic violence victims.
+        </div>
         <button className="bg-[rgba(132,62,250,0.3)] font-herfonty transition ease-in-out hover:bg-[rgba(132,62,250,0.9)] mt-5 my-8 text-purple text-3xl font-bold py-10 px-10 w-[17rem] rounded mx-auto">
           Go to forum
         </button>
-        </div>
+      </div>
 
+      <button
+        className="bg-[rgba(132,62,250,0.3)] font-herfonty transition ease-in-out hover:bg-[rgba(132,62,250,0.9)] mt-5 my-8 text-purple text-3xl font-bold py-10 px-10 w-[17rem] rounded mx-auto"
+        onClick={() => setIsModalOpen(true)}
+      >
+        Add Post
+      </button>
 
-              <button className="bg-[rgba(132,62,250,0.3)] font-herfonty transition ease-in-out hover:bg-[rgba(132,62,250,0.9)] mt-5 my-8 text-purple text-3xl font-bold py-10 px-10 w-[17rem] rounded mx-auto"
-        onClick={() => setIsModalOpen(true)}>Add Post</button>
-
-              
-              {/* {conditional render please <AddPost />} */}
-            {isModalOpen && <Add Post closeModal={() => setIsModalOpen(false)} />}
-            </div>
+      {/* {conditional render please <AddPost />} */}
+      {isModalOpen && <Add Post closeModal={() => setIsModalOpen(false)} />}
+    </div>
   );
-          }
-  
-          
+}
+
 export default Home;
