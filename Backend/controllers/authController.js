77c37f8647aeca89d3e6b9522164d6bfbd923113
aarushpatil <aarushpatil4@gmail.com>
2024-02-03 14:49:32 -0500

    authController

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -4,7 +4,12 @@ const {StatusCodes} = require("http-status-codes");
 const {attachCookiesToResponse, createTokenUser} = require("../utils");
 
 const register = async (req, res) => {
-    const { profileImage, name, password } = req.body;
+    const { profileImage, username, password } = req.body;
 
-    const isFirstAccount = (await User.countDocuments({})) === 0;
+    const user = await userData.create({username, password});
+    const tokenUser = createTokenUser(user);
+
+    attachCookiesToResponse({ res, user: tokenUser});
+    res.status(200).json({user: tokenUser});
 }
+
