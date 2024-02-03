11ae0338ab743da514ae2384d7a3651a41abe4e5
johannaj16 <johannaj@vt.edu>
2024-02-03 12:15:34 -0500

    homepage

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -3,8 +3,8 @@ function Home() {
   return (
     <>
     <div className="flex flex-col items-center justify-center h-screen">
-      <div className="w-full text-4xl text-center py-20">FreedomHER</div>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-10 px-10 w-1/2 rounded mx-auto">
+      <div className="w-full text-7xl text-center py-20">FreedomHER</div>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white text-xl font-bold py-10 px-10 w-1/2 rounded mx-auto">
         Go to forum
       </button>
     </div>
