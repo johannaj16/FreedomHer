378a93bc016b6e063a493d7c755622560e525ef8
aarushpatil <aarushpatil4@gmail.com>
2024-02-03 13:08:27 -0500

    added a route

---
+++ Backend/models/postData.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    vote: {
+        type: Number,
+        required: false
+    },
+    genre: {
+        type: String,
+        required: true
+    },
+    comments: {
+        type: [mongoose.Schema.Types.ObjectId],
+        required: true,
+        ref: "FreedomHerReply",
+        default: []
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "FreedomHerUser",
+    }
+}, {timestamps: true});
+
+const postData = mongoose.model('FreedomHerPost', postSchema);
+module.exports = postData;
