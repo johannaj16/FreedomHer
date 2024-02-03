94600321cb4da84074c42ce877e4c186391cb9ee
Adanato <Adanatonw@gmail.com>
2024-02-03 15:19:50 -0500

    fixing stuff

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -6,8 +6,8 @@ const { attachCookiesToResponse, createTokenUser } = require("../utils");
 const register = async (req, res) => {
   const { profileImage, username, password } = req.body;
 
-    const user = await userData.create({profileImage, username, password});
-    const tokenUser = createTokenUser(user);
+  const user = await userData.create({ profileImage, username, password });
+  const tokenUser = createTokenUser(user);
 
   attachCookiesToResponse({ res, user: tokenUser });
   res.status(200).json({ user: tokenUser });
@@ -19,12 +19,12 @@ const login = async (req, res) => {
   if (!username || !password) {
     throw new CustomError.BadRequestError("Please provide email and password");
   }
-  const userData = await User.findOne({ username });
 
   if (!username) {
     throw new CustomError.UnauthenticatedError("Invalid Credentials");
   }
-  const isPasswordCorrect = await user.comparePassword(password);
+  const user = await userData.findOne({ username });
+  const isPasswordCorrect = await userData.comparePassword(password);
   if (!isPasswordCorrect) {
     throw new CustomError.UnauthenticatedError("Invalid Credentials");
   }
@@ -35,15 +35,14 @@ const login = async (req, res) => {
 };
 
 const logout = async (req, res) => {
-    req.cookie("token", "logout", {
-        httpOnly: true,
-        expires: new Date(Date.now()),
-    })
+  req.cookie("token", "logout", {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
 };
 
-
 module.exports = {
-    register,
-    login,
-    logout,
+  register,
+  login,
+  logout,
 };
