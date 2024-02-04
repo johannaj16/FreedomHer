6e1c224f11219a4a48b41423a53fe464f803d33a
Adanato <Adanatonw@gmail.com>
2024-02-04 09:26:40 -0500

    uncommenting comments for posts

--- Backend/utils/jwt.js
+++ Backend/utils/jwt.js
@@ -23,9 +23,9 @@ const attachCookiesToResponse = ({ res, user }) => {
   const oneDay = 1000 * 60 * 60 * 24;
 
   res.cookie("token", token, {
-    httpOnly: false, // To prevent XSS attacks
+    httpOnly: process.env.NODE_ENV === "production", // To prevent XSS attacks
     expires: new Date(Date.now() + oneDay), // Cookie expiration
-    secure: false, // Set to true in production
+    secure: process.env.NODE_ENV === "production", // Set to true in production
     signed: true, // If using signed cookies
     sameSite: "Strict", // Adjust depending on cross-site access needs
   });
