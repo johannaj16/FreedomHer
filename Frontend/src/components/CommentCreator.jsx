afe0f4b8d6a30b318cb239dd2b0b713641817fc3
GabePalomino <gabepalominoc@gmail.com>
2024-02-04 05:53:06 -0500

    Added form to the comment section

--- Frontend/src/components/CommentCreator.jsx
+++ Frontend/src/components/CommentCreator.jsx
@@ -1,20 +1,27 @@
 import React from "react";
 
 function CommentCreator() {
+  const handleSubmit = () => {
+    console.log("something");
+  };
+
   return (
     <>
       <div>
-        <div className="flex flex-row md:items-start items-center p-10 text-xl">
+        <form className="flex flex-row md:items-start items-center p-10 text-xl">
           <textarea
             class="bg-gray-100 shadow-lg rounded font-herfonty resize-none w-full h-20 p-3 placeholder-gray-500 focus:outline-none focus:bg-white"
             name="body"
             placeholder="Your reply"
             required
           ></textarea>
-          <button className="font-herfonty text-lg bg-[rgba(135,116,162,0.4)] transition ease-in-out hover:bg-[rgba(135,116,162,0.85)] py-4 px-3 rounded-lg sm:w-[10rem] mt-4 ml-3">
+          <button
+            onSubmit={handleSubmit}
+            className="font-herfonty text-lg bg-[rgba(135,116,162,0.4)] transition ease-in-out hover:bg-[rgba(135,116,162,0.85)] py-4 px-3 rounded-lg sm:w-[10rem] mt-4 ml-3"
+          >
             Post reply
           </button>
-        </div>
+        </form>
       </div>
     </>
   );
